f6569adced1546ae5228fa4acdb2e30d63c9bc0b
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2014-02-20 18:09:48 +0400

    initial commit

---
+++ mapsdu/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from geonode.urls import *
+
+from django.views.generic import RedirectView
+
+urlpatterns = patterns('',
+
+    # Static pages
+#    url(r'^$', 'geonode.views.index', {'template': 'site_index.html'}, name='home'),
+    url(r'^$', RedirectView.as_view(url='/layers/')),
+ ) + urlpatterns
